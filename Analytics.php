cc94b404350be3109b612aec255f00424f25eb0d
Jan-Nicklas Adler <jan.nicklas.adler@gmail.com>
2022-02-26 14:10:06 +0100

    Empty files needed later

---
+++ Analytics.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CompleteAnalytics;
+
+use CompleteAnalytics\Tests\AnalyticsTest;
+
+/**
+ * Class Analytics. Manages all existing analytics integrations and helps to select a suitable one.
+ *
+ * @package CompleteAnalytics
+ * @author Jan-Nicklas Adler
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ * @see AnalyticsTest phpunit test
+ */
+class Analytics
+{
+
+}
---
+++ Tests/AnalyticsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CompleteAnalytics\Tests;
+
+use CompleteAnalytics\Analytics;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests the Analytics class.
+ *
+ * @package CompleteAnalytics
+ * @author Jan-Nicklas Adler
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ * @see Analytics Tested class
+ */
+class AnalyticsTest extends TestCase
+{
+
+}
