25debc3fee4b2b786be81d93a953fd5071b2d9f2
Jan-Nicklas Adler <jan.nicklas.adler@gmail.com>
2022-03-08 19:58:44 +0100

    Additional Helper Functions

---
+++ Core/HelperFunctions.php
@@ -0,0 +1,126 @@
+<?php
+
+
+namespace ScAnalytics\Core;
+
+
+use RuntimeException;
+
+/**
+ * Class HelperFunctions. Useful helper functions.
+ *
+ * @author Jan-Nicklas Adler
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ * @copyright All Rights Reserved.
+ */
+class HelperFunctions
+{
+
+    /**
+     * Returns the path to the project root folder, where the composer.json and the vendor directory are located. Does <b>not</b> include a trailing slash. Also works with unit tests.
+     *
+     * @return string Directory path of the project root
+     */
+    public static function getRoot(): string
+    {
+        $directory = __DIR__;
+        $root = null;
+        do {
+            $directory = dirname($directory);
+            $vendor = $directory . '/vendor';
+            if (file_exists($vendor)) {
+                $root = $directory;
+            }
+        } while (is_null($root) && $directory !== '/');
+        if (!is_null($root)) {
+            return $root;
+        }
+        throw new RuntimeException("Could not get project root directory");
+    }
+
+    /**
+     * Locates the directory storing the assets. Also works with unit tests.
+     *
+     * @return string Directory path of the asset directory
+     */
+    public static function getAssetsDir(): string
+    {
+        $root = self::getRoot();
+        if (file_exists($root . "/Assets")) {
+            return $root . "/Assets";
+        }
+
+        if (file_exists($root . "/libraries/promatur/sc-analytics")) {
+            return $root . "/libraries/promatur/sc-analytics";
+        }
+
+        throw new RuntimeException("Could not get asset directory");
+    }
+
+    /**
+     * Checks, if the connection is secured over HTTPS. Works for most load balancers.
+     * @return bool Boolean, if the connection uses HTTPS
+     */
+    public static function isHTTPS(): bool
+    {
+        $isSecure = false;
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+            $isSecure = true;
+        } elseif ((!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') || (!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] === 'on')) {
+            $isSecure = true;
+        } elseif (!empty($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT']) === 443) {
+            $isSecure = true;
+        }
+        return $isSecure;
+    }
+
+    /**
+     * Gets the domain, to user is currently on.
+     *
+     * @return string Domain of the user
+     */
+    public static function getDomain(): string
+    {
+        $s = self::isHTTPS() ? "s" : "";
+        $sp = strtolower($_SERVER["SERVER_PROTOCOL"] ?? "HTTP/1.1");
+        $protocol = explode('/', $sp)[0]. $s;
+        return $protocol . "://" . ($_SERVER['SERVER_NAME'] ?? "UNKNOWN");
+    }
+
+    /**
+     * Gets the full URL, to user is currently on.
+     *
+     * @return string Full URL
+     */
+    public static function getURL(): string
+    {
+        $requestURI = $_SERVER['REQUEST_URI'] ?? "";
+        return self::getDomain() . $requestURI;
+    }
+
+    /**
+     * Returns the IP of the user by accessing different methods. Value <b>cannot be trusted</b> by 100%. Supports CloudFlare.
+     *
+     * @return string|null IP of the current user
+     */
+    public static function getIpAddress(): ?string
+    {
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+            if (array_key_exists($key, $_SERVER)) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    $ip = trim($ip);
+
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
---
+++ Tests/Core/HelperFunctionsTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace ScAnalytics\Tests\Core;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use ScAnalytics\Core\HelperFunctions;
+
+/**
+ * Tests the HelperFunctions class.
+ *
+ * @author Jan-Nicklas Adler
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ * @copyright All Rights Reserved.
+ */
+class HelperFunctionsTest extends TestCase
+{
+
+    protected function tearDown(): void
+    {
+        unset($_SERVER['HTTPS'], $_SERVER['SERVER_PORT'], $_SERVER['HTTP_X_FORWARDED_PROTO'], $_SERVER['HTTP_X_FORWARDED_SSL'], $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']);
+    }
+
+    public function testGetRoot(): void
+    {
+        self::assertNotEmpty(HelperFunctions::getRoot());
+    }
+
+    public function testGetAssetsDir(): void
+    {
+        self::assertNotEmpty(HelperFunctions::getAssetsDir());
+        self::assertStringContainsString("/Assets", HelperFunctions::getAssetsDir());
+    }
+
+    public function testIsHTTPS(): void
+    {
+        $tests = [
+            ["off", 80, "http", "off", false],
+            ["on", 443, "https", "on", true],
+            [null, 443, null, null, true],
+            [null, 80, "http", null, false],
+            [null, 80, "https", null, true],
+            [null, 80, "http", "on", true]
+        ];
+        $variables = ['HTTPS', 'SERVER_PORT', 'HTTP_X_FORWARDED_PROTO', 'HTTP_X_FORWARDED_SSL'];
+
+        foreach ($tests as $tid => $test) {
+            foreach ($variables as $i => $variable) {
+                if (is_null($test[$i]) && isset($_SERVER[$variable])) {
+                    unset($_SERVER[$variable]);
+                } else {
+                    $_SERVER[$variable] = $test[$i];
+                }
+            }
+
+            self::assertEquals($test[4], HelperFunctions::isHTTPS(), "Failed test for #" . $tid . " " . print_r($test, true));
+        }
+    }
+
+    /** @noinspection HttpUrlsUsage */
+    public function testGetDomain(): void
+    {
+        self::assertEquals("http://UNKNOWN", HelperFunctions::getDomain());
+
+        $_SERVER['HTTPS'] = "on";
+        self::assertEquals("https://UNKNOWN", HelperFunctions::getDomain());
+
+        $_SERVER['SERVER_NAME'] = "promatur.com";
+        self::assertEquals("https://promatur.com", HelperFunctions::getDomain());
+
+        $_SERVER['HTTPS'] = "off";
+        self::assertEquals("http://promatur.com", HelperFunctions::getDomain());
+    }
+
+    public function testGetURL(): void
+    {
+        self::assertEquals("http://UNKNOWN", HelperFunctions::getURL());
+
+        $_SERVER['HTTPS'] = "on";
+        $_SERVER['SERVER_NAME'] = "promatur.com";
+        $_SERVER['REQUEST_URI'] = "/abc";
+        self::assertEquals("https://promatur.com/abc", HelperFunctions::getURL());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetIpAddress(): void {
+        self::assertNull(HelperFunctions::getIpAddress());
+        $parameters = ['HTTP_CLIENT_IP', 'HTTP_CF_CONNECTING_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR'];
+        foreach ($parameters as $key) {
+            $valid = "56.148.194.76";
+            $_SERVER[$key] = $valid;
+            self::assertEquals($valid, HelperFunctions::getIpAddress(), "Failed for $key");
+            $_SERVER[$key] = "56.148.194";
+            self::assertNull(HelperFunctions::getIpAddress());
+            unset($_SERVER[$key]);
+        }
+    }
+}
